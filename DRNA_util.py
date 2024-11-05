1cb1cf7a39af669e2ddc342e38061a8099d4b892
Jouke Jonathan van der Lek <j.j.vanderlek@students.uu.nl>
2024-11-05 13:09:23 +0100

    Large refactoring, cant be bothered to type it out.

--- DRNA_util.py
+++ DRNA_util.py
@@ -4,44 +4,80 @@ NUCLEOTIDES_RNA = ['A','C','G','U']
 # TODO Combine isRna and isDNA into one, refactor code so the standard
 # becomes DNA = True and RNA = FALSE. In this way any sequence can be identified by a boolean.
 
-def initializeSequence(sequence, ignore = ['\n']) -> tuple:
-    """Returns both the nucleic acid type; DNA = True, RNA = False. And the counts of each nucleotide store in a dictionary.
-    The first element of the tuple is the type boolean and the second is the dictionary.
+def analyzeString(sequence, ignore = ['\n']) -> dict:
+    """Analyses any given iterable sequence of characters. Returns its properties as a dict.
 
     Args:
         sequence (list): A string, list or tuple of a nucleic acid
         ignore (list, optional): A string, list or tuple of characters to ignore when considering nucleic acid type. Ignores '\\n' by default.
 
     Returns:
-        tuple: (type boolean DNA or RNA, nucleotide_count dictionary)
+        list:  List of sequence information\n
+     0 - type specifier: DNA 'D', RNA 'R' or NOT Nucleic Acid ''\n
+     1 - Dictionary for keeping track of nucleotide count
     """
     assert hasattr(sequence, '__getitem__'), "sequence argument not subscriptable"
     if ignore: assert hasattr(ignore, '__getitem__'), "ignore argument not subscriptable"
     if not ignore: ignore = []
 
-    # Dictionary for keeping track of nucleotide count
-    nucleotide_counts = {
+    # List of sequence information
+    # 0 - type specifier: DNA 'D', RNA 'R' or NOT Nucleic Acid ''
+    # 1 - Dictionary for keeping track of nucleotide count
+    sequence_info = ['',{
         'A' : 0,
         'C' : 0,
         'G' : 0,
         'T' : 0,
         'U' : 0
-    }
+    }]
     # Iterates each element in the list, adds nucleotides to dict. If any aren't valid return False
     for char in sequence:
         if char in ignore:
             continue
         if char in NUCLEOTIDES_DNA + NUCLEOTIDES_RNA:
-            nucleotide_counts[char] += 1
+            sequence_info[1][char] += 1
             continue
         else:
-            return (None, None)
+            return sequence_info
     
-    # The program should not throw an error if it is neither DNA or RNA, but ill figure that out later
-    assert nucleotide_counts['T'] * nucleotide_counts['U'] == 0, "Sequence contains both 'T' and 'U'"
-    # Decide on DNA (True) or RNA (False) based on 'T' and 'U' counts
-    if nucleotide_counts['T'] > 0 and nucleotide_counts['U'] <= 0: return (True, nucleotide_counts)
-    if nucleotide_counts['U'] > 0 and nucleotide_counts['T'] <= 0: return (False, nucleotide_counts)
+    
+    # Decide on DNA (D) or RNA (R) based on 'T' and 'U' counts
+    if sequence_info[1]['T'] > 0 and sequence_info[1]['U'] <= 0: sequence_info[0] = 'D'
+    if sequence_info[1]['U'] > 0 and sequence_info[1]['T'] <= 0: sequence_info[0] = 'R'
 
-    # Safe guards so that function always returns something
-    return (None, None)
+    return sequence_info
+
+class Sequence:
+    """Central sequence object of DRNA-Utils. 
+    """
+    def __init__(self) -> None:
+        self.type = ''
+        self.nucleotides = {
+            'A': 0,
+            'C': 0,
+            'G': 0,
+            'T': 0,
+            'U': 0
+        }
+
+    def readFromFile(self, filename, ignore) -> int:
+        try:
+            with open(filename, 'r') as read_file:
+                file_content = read_file.readlines()
+                last_type = ''
+                for line_number, line in enumerate(file_content):
+                    line_info = analyzeString(line, ignore)
+                    # See if the nucleic acid type changed since the last line (excluding the first line)
+                    # If so exit. TODO: Exit more gracefully I.e not a half filled variable
+                    if last_type != line_info[0] and line_number > 0:
+                        print("Could not determine nucleic acid type in readFromFile()")
+                        return 1
+                    self.nucleotides = {k: self.nucleotides.get(k, 0) + line_info[1].get(k, 0) for k in set(self.nucleotides) | set(line_info[1])}
+                    last_type = line_info[0]
+                # If no nucleic acid type mismatches were found, meaning last_type stayed constant, assign nucleic acid type.
+                self.type = last_type
+            return 0
+        except FileNotFoundError:
+            print("No file with name %s was found..." % filename)
+            input()
+            return 1
--- reaDNA.py
+++ reaDNA.py
@@ -11,28 +11,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument('filename')
 args = parser.parse_args()
 
-# Check to see if file exists, otherwise exist with code 1
-file_lines = []
-try:
-    with open(args.filename, 'r') as read_file:
-        file_content = read_file.readlines()
-        
-        NUCLEIC_TYPE : bool = None
-        NUCLEIC_COUNT : dict = {}
-        for line in file_content:
-            temp = DRNA_util.initializeSequence(line)
-            NUCLEIC_TYPE = temp[0]
-            NUCLEIC_COUNT= {k: NUCLEIC_COUNT.get(k, 0) + temp[1].get(k,0) for k in set(NUCLEIC_COUNT) | set(temp[1])}
-        
-            
-except FileNotFoundError:
-    print("No file with name %s was found..." % args.filename)
-    input()
-    exit(1)
-
-
-print(NUCLEIC_COUNT)
-
-
+seq1 = DRNA_util.Sequence()
+seq1.readFromFile(args.filename, ['\n'])
+print(seq1.type)
 # Input call to prevent terminal from closing
 input("Press RETURN to close...")
