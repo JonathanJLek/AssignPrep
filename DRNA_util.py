5cb91fb157f191896f77ffe0d27e07152a206723
Jouke Jonathan van der Lek <j.j.vanderlek@students.uu.nl>
2024-11-05 13:21:15 +0100

    Comments and refactoring

--- DRNA_util.py
+++ DRNA_util.py
@@ -1,6 +1,10 @@
 NUCLEOTIDES_DNA = ['A','C','G','T']
 NUCLEOTIDES_RNA = ['A','C','G','U']
 
+DNA_TYPE = 'D'
+RNA_TYPE = 'R'
+NON_TYPE = ''
+
 # TODO Combine isRna and isDNA into one, refactor code so the standard
 # becomes DNA = True and RNA = FALSE. In this way any sequence can be identified by a boolean.
 
@@ -23,7 +27,7 @@ def analyzeString(sequence, ignore = ['\n']) -> dict:
     # List of sequence information
     # 0 - type specifier: DNA 'D', RNA 'R' or NOT Nucleic Acid ''
     # 1 - Dictionary for keeping track of nucleotide count
-    sequence_info = ['',{
+    sequence_info = [NON_TYPE,{
         'A' : 0,
         'C' : 0,
         'G' : 0,
@@ -51,7 +55,7 @@ class Sequence:
     """Central sequence object of DRNA-Utils. 
     """
     def __init__(self) -> None:
-        self.type = ''
+        self.type = NON_TYPE
         self.nucleotides = {
             'A': 0,
             'C': 0,
@@ -61,17 +65,31 @@ class Sequence:
         }
 
     def readFromFile(self, filename, ignore) -> int:
+        """Reads a .txt file for nucleotides using DRNA_util.analyzeString().
+
+        Args:
+            filename (string): File to read.
+            ignore (list): A string, list or tuple of characters to ignore when considering nucleic acid type. Ignores '\\n' by default.
+
+        Returns:
+            int: exitcode \n
+            0 - success,
+            1 - failed
+        """
         try:
             with open(filename, 'r') as read_file:
                 file_content = read_file.readlines()
-                last_type = ''
+                # Variable to keep track of nucleic acid type between lines.
+                last_type = NON_TYPE
                 for line_number, line in enumerate(file_content):
                     line_info = analyzeString(line, ignore)
                     # See if the nucleic acid type changed since the last line (excluding the first line)
-                    # If so exit. TODO: Exit more gracefully I.e not a half filled variable
+                    # If so, exit. TODO: Exit more gracefully I.e not a half filled self.nucleotides
                     if last_type != line_info[0] and line_number > 0:
                         print("Could not determine nucleic acid type in readFromFile()")
                         return 1
+                    # Merge dictionaries of nucleotides count with old values and new line_info values. 
+                    # line_info[NUCLEIC ACID TYPE, dict]
                     self.nucleotides = {k: self.nucleotides.get(k, 0) + line_info[1].get(k, 0) for k in set(self.nucleotides) | set(line_info[1])}
                     last_type = line_info[0]
                 # If no nucleic acid type mismatches were found, meaning last_type stayed constant, assign nucleic acid type.
@@ -79,5 +97,4 @@ class Sequence:
             return 0
         except FileNotFoundError:
             print("No file with name %s was found..." % filename)
-            input()
             return 1
